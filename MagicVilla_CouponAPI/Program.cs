7665a7dacffd364c0bf62f7ee3702677fb5ae09b
hrodr <hrodr@HROD-LAPTOP-WIN>
2023-08-03 22:50:30 -0500

    Add project files.

---
+++ MagicVilla_CouponAPI/Program.cs
@@ -0,0 +1,91 @@
+using AutoMapper;
+using FluentValidation;
+using MagicVilla_CouponAPI;
+using MagicVilla_CouponAPI.Data;
+using MagicVilla_CouponAPI.Models;
+using MagicVilla_CouponAPI.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(MappingConfig));
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/api/coupon", (ILogger<Program> _logger) =>
+{
+    APIResponse response = new();
+    _logger.Log(LogLevel.Information, "Getting all coupons");
+    response.Result = CouponStore.couponList;
+    response.IsSuccess = true;
+    response.StatusCode = HttpStatusCode.OK;
+
+    return Results.Ok(response);
+}).WithName("GetCoupons").Produces<APIResponse>(200);
+
+app.MapGet("/api/coupon/{id:int}", (int id) =>
+{
+    APIResponse response = new();
+    response.Result = CouponStore.couponList.FirstOrDefault(u => u.Id == id);
+    response.IsSuccess = true;
+    response.StatusCode = HttpStatusCode.OK;
+    return Results.Ok(response);
+}).WithName("GetCoupon").Produces<Coupon>(200);
+
+app.MapPost("/api/coupon", async (IMapper _mapper,
+    IValidator<CouponCreateDTO> _validation, [FromBody] CouponCreateDTO coupon_C_DTO) =>
+{
+    var validationResult = await _validation.ValidateAsync(coupon_C_DTO);
+
+    if (!validationResult.IsValid)
+    {
+        return Results.BadRequest(validationResult.Errors.FirstOrDefault().ToString());
+    }
+
+    if (CouponStore.couponList.FirstOrDefault(u => u.Name.ToLower() == coupon_C_DTO.Name.ToLower()) != null)
+    {
+        return Results.BadRequest("Coupon Name already exists");
+    }
+
+    Coupon coupon = _mapper.Map<Coupon>(coupon_C_DTO);
+
+    coupon.Id = CouponStore.couponList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+    CouponStore.couponList.Add(coupon);
+
+    CouponDTO couponDTO = _mapper.Map<CouponDTO>(coupon);
+
+    return Results.CreatedAtRoute("GetCoupon", new { id = coupon.Id }, couponDTO);
+
+    // return Results.Created($"/api/coupon/{coupon.Id}",coupon);
+}
+).WithName("CreateCoupon").Accepts<CouponCreateDTO>("application/json").Produces<CouponDTO>(201).Produces(400);
+
+app.MapPut("/api/coupon", () =>
+{
+
+}
+).WithName("UpdateCoupon");
+
+app.MapDelete("/api/coupon/{id:int}", (int id) =>
+{
+
+}
+).WithName("DeleteCoupon");
+
+app.Run();
+
